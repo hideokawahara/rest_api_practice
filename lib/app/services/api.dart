79d572320d40adb7c70ca10f8080819c0dc4de1a
河原日出夫 <kawahide.72@gmail.com>
2021-07-03 21:46:52 +0900

    feat: api追加

---
+++ lib/app/services/api.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:rest_api_practoce/app/services/api_keys.dart';
+
+class API {
+  API({@required this.apiKey});
+
+  final String apiKey;
+
+  factory API.sandbox() => API(apiKey: APIKeys.ncovSandboxKey);
+
+  static final String host = 'ncov2019-admin.firebaseapp.com';
+
+  Uri tokenUri() => Uri(
+        scheme: 'https',
+        host: host,
+        path: 'token',
+      );
+}
