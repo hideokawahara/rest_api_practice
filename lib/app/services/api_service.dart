d75841ea9c16145e0a8b7dfa3a1bf82894eb1408
河原日出夫 <kawahide.72@gmail.com>
2021-07-04 22:56:46 +0900

    feat: データ取得後の処理の追加

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+
 import 'api.dart';
 import 'package:http/http.dart' as http;
 
@@ -23,4 +25,37 @@ class APIService {
         'Request ${api.tokenUri()} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
     throw response;
   }
+
+  Future<int> getEndpointData({
+    @required String accessToken,
+    Endpoint endpoint,
+  }) async {
+    final uri = api.endpointUri(endpoint);
+    final response = await http.get(
+      uri,
+      headers: {'Authorization': 'Basic $accessToken'},
+    );
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      if (data.isNotEmpty) {
+        final Map<String, dynamic> endpointData = data[0];
+        final String responseJsonKey = _responseJsonKeys[endpoint];
+        final int result = endpointData[responseJsonKey];
+        if (result != null) {
+          return result;
+        }
+      }
+    }
+    print(
+        'Request $uri failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
+    throw response;
+  }
+
+  static Map<Endpoint, String> _responseJsonKeys = {
+    Endpoint.cases: 'cases',
+    Endpoint.casesSuspected: 'data',
+    Endpoint.casesConfirmed: 'data',
+    Endpoint.deaths: 'data',
+    Endpoint.recovered: 'data',
+  };
 }
