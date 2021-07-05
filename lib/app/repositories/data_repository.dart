6f14d800d13f8484b28f002779d8c400a098c805
河原日出夫 <kawahide.72@gmail.com>
2021-07-05 23:54:43 +0900

    feat: リポジトリ作成

---
+++ lib/app/repositories/data_repository.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:rest_api_practoce/app/services/api.dart';
+import 'package:rest_api_practoce/app/services/api_service.dart';
+
+class DataRepository {
+  DataRepository(@required this.apiService);
+  final APIService apiService;
+
+  String _accessToken;
+
+  Future<int> getEndpointData(Endpoint endpoint) async {
+    try {
+      if (_accessToken != null) {
+        _accessToken = await apiService.getAccessToken();
+      }
+
+      return await apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: endpoint);
+    } on Response catch (response) {
+      if (response.statusCode == 402) {
+        _accessToken = await apiService.getAccessToken();
+        return await apiService.getEndpointData(
+            accessToken: _accessToken, endpoint: endpoint);
+      }
+      rethrow;
+    }
+  }
+}
