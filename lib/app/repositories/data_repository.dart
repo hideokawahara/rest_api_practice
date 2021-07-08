f85f44f64b243739784ac5cea21746ffe34a9234
河原日出夫 <kawahide.72@gmail.com>
2021-07-08 23:08:12 +0900

    feat: future list create

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:rest_api_practoce/app/repositories/endpoints_data.dart';
 import 'package:rest_api_practoce/app/services/api.dart';
 import 'package:rest_api_practoce/app/services/api_service.dart';
 
@@ -24,5 +25,42 @@ class DataRepository {
       }
       rethrow;
     }
+  }
+
+  Future<EndpointsData> getAllEndpointsData() async {
+    try {
+      if (_accessToken == null) {
+        _accessToken = await apiService.getAccessToken();
+      }
+      return await _getAllEndpointsData();
+    } on Response catch (response) {
+      if (response.statusCode == 401) {
+        _accessToken = await apiService.getAccessToken();
+        return await _getAllEndpointsData();
+      }
+      rethrow;
+    }
+  }
+
+  Future<EndpointsData> _getAllEndpointsData() async {
+    final values = await Future.wait([
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.cases),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.casesSuspected),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.casesConfirmed),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.deaths),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.recovered),
+    ]);
+    return EndpointsData(values: {
+      Endpoint.cases: values[0],
+      Endpoint.casesSuspected: values[1],
+      Endpoint.casesConfirmed: values[2],
+      Endpoint.deaths: values[3],
+      Endpoint.recovered: values[4],
+    });
   }
 }
