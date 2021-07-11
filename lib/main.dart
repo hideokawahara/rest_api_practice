26fa69b32fb4241d3e680786fe2e47a7dfc5d480
河原日出夫 <kawahide.72@gmail.com>
2021-07-11 15:46:00 +0900

    feat: 時間を日本にした

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+// import 'package:intl/date_symbol_data_file.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 import 'package:rest_api_practoce/app/repositories/data_repository.dart';
 import 'package:rest_api_practoce/app/services/api.dart';
 import 'package:rest_api_practoce/app/services/api_service.dart';
 import 'package:rest_api_practoce/app/ui/dashboard.dart';
 
-void main() {
+void main() async {
+  Intl.defaultLocale = 'ja_JP';
+  // Intl.withLocale('ja_JP', () => runApp(MyApp()));
+  // await initializeDateFormatting('ja_JP', null);
+  await initializeDateFormatting();
+  print(DateTime.now().toIso8601String());
   runApp(MyApp());
 }
 
